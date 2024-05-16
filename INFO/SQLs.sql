4cdfccdfc2838c15fa0ab34024644f5ab7abd8ba
Rasti1 <rostrostov1999@gmail.com>
2024-05-16 18:55:49 +0300

    Fix pom.xml and sql-table commit

--- INFO/SQLs.sql
+++ INFO/SQLs.sql
@@ -2,7 +2,7 @@ CREATE DATABASE demo_db;
 
 CREATE TABLE IF NOT EXISTS products (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    productName VARCHAR(255) NOT NULL,
     quota INT NOT NULL,
     price DECIMAL(10, 2) NOT NULL
 );
